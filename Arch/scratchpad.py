39bda31f4dba2123dc988569ad25d2a5c863ed91
Tom Tarpey <tomtarpeydev@gmail.com>
2021-03-26 01:06:07 +0000

    end of part 2 arch

--- Arch/scratchpad.py
+++ Arch/scratchpad.py
@@ -1,26 +1,50 @@
-# add_to_pc?
+# # add_to_pc?
 
-# LDI 
-# 1
-# 1 
-# PRN <--- pc
-# 2
-LDI = 0b10000010
-HLT = 0b00000001
-pc = 0
-mem = [
-    LDI,
-    2,
-    3,
-    HLT
-]
-# opperand_size = LDI >> 6
-# add_to_pc = opperand_size + 1
+# # LDI 
+# # 1
+# # 1 
+# # PRN <--- pc
+# # 2
+# LDI = 0b10000010
+# HLT = 0b00000001
+# pc = 0
+# mem = [
+#     LDI,
+#     2,
+#     3,
+#     HLT
+# ]
+# # opperand_size = LDI >> 6
+# # add_to_pc = opperand_size + 1
 
-# DECODE
-inst = mem[pc]
-add_to_pc = (inst >> 6) + 1
-# print(opperand_size)
+# # DECODE
+# inst = mem[pc]
+# add_to_pc = (inst >> 6) + 1
+# # print(opperand_size)
 
-pc += add_to_pc
-print(mem[pc])
+# pc += add_to_pc
+# print(mem[pc])
+
+import sys
+if len(sys.argv) != 2:
+    # print the usage?
+    print(f"Usage: {sys.argv[0]} <filename>")
+    # exit
+    sys.exit(1)
+
+file_name = sys.argv[1]
+# print(file_name)
+try:
+    with open(file_name) as f:
+        for line in f:
+            l = line.split('#')
+            sanitized_l = l[0].strip()
+            if sanitized_l == '':
+                continue
+            # print(l)
+            print(int(sanitized_l, 2))
+            # print(int(line, 2))
+
+except FileNotFoundError:
+    print(f"{sys.argv[0]}: {sys.argv[1]} not found.")
+    sys.exit(2)
--- Arch/simple.py
+++ Arch/simple.py
@@ -1,3 +1,4 @@
+import sys
 # components of a computer,
 # What is a computer to us?
 
@@ -24,31 +25,63 @@ STORE      = 0b00010001 # store a number from the ram 2 byte ahead of the instru
 PRINT_REG  = 0b01111101 # print a number at the index in to registers, provided by the number from current instruction + 1 in ram
 ADD        = 0b01101101 # adds the num stored at the register at the index from ram 1 byte away from the instruction
 
-# list for the ram.
-ram = [
-    PRINT_TOM,
-    PRINT_NUM,
-    23,
-    STORE,
-    1,
-    10,
-    STORE,
-    3,
-    20,
-    PRINT_REG,
-    1,
-    PRINT_REG,
-    3,
-    ADD,
-    1,
-    3,
-    PRINT_REG,
-    1,
-    PRINT_REG,
-    3,
-    HALT
-]
+ram = [0] * 256
 
+def load_mem(filename):
+    try:
+        addr = 0
+        with open(filename) as f:
+            for line in f:
+                # split the line on #
+                comment_split = line.split('#')
+                # strip the data
+                data = comment_split[0].strip()
+
+                # deal with empty line
+                if data == '':
+                    continue
+
+                # extract the value (convert the number to an int).
+                val = int(data, 2)
+
+                # store the val in ram at current address?
+                ram[addr] = val
+
+                # increment address
+                addr += 1
+    except FileNotFoundError:
+        print(f"{sys.argv[0]}: {sys.argv[1]} not found.")
+        sys.exit(2)
+        
+# list for the ram.
+# ram = [
+#     PRINT_TOM,
+#     PRINT_NUM,
+#     23,
+#     STORE,
+#     1,
+#     10,
+#     STORE,
+#     3,
+#     20,
+#     PRINT_REG,
+#     1,
+#     PRINT_REG,
+#     3,
+#     ADD,
+#     1,
+#     3,
+#     PRINT_REG,
+#     1,
+#     PRINT_REG,
+#     3,
+#     HALT
+# ]
+if len(sys.argv) != 2:
+    print(f"Usage: {sys.argv[0]} <filename>")
+    sys.exit(1)
+
+load_mem(sys.argv[1])
 # a program counter to keep track of what we are executing.
 pc = 0
 
